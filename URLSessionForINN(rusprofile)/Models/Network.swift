6af01364ac52807f34ab913d02c7ef2798f43478
Isa199510 <isa.arbuhov@mail.ru>
2022-11-20 22:15:55 +0600

    setup

--- URLSessionForINN(rusprofile)/Models/Network.swift
+++ URLSessionForINN(rusprofile)/Models/Network.swift
@@ -11,22 +11,22 @@ enum Link: String {
 
 
 
-struct INN: Decodable {
-    let ips: [IndPr]
-}
-
-struct IndPr: Decodable {
-    let name: String
-    let sex: Sex
-    let ogrnip: String
-    let inn: String
-    let region_name: String
-    let main_okved_id: String
-    let okved_descr: String
-    let reg_date: String
-}
-
-enum Sex: Int, Decodable {
-    case man = 1
-    case woman = 2
-}
+//struct INN: Decodable {
+//    let ips: [IndPr]
+//}
+//
+//struct IndPr: Decodable {
+//    let name: String
+//    let sex: Sex
+//    let ogrnip: String
+//    let inn: String
+//    let region_name: String
+//    let main_okved_id: String
+//    let okved_descr: String
+//    let reg_date: String
+//}
+//
+//enum Sex: Int, Decodable {
+//    case man = 1
+//    case woman = 2
+//}
