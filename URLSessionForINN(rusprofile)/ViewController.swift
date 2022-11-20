efb00e04f9f3e207dee0eda6552552c547e8312e
Isa199510 <isa.arbuhov@mail.ru>
2022-11-20 19:21:23 +0600

    Initial Commit

---
+++ URLSessionForINN(rusprofile)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionForINN(rusprofile)
+//
+//  Created by Иса on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
